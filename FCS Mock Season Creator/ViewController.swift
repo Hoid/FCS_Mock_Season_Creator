22b70b841fb6e3580fdefe6f340f9b24e44dc5c3
Tyler Cheek <tcheek@bandwidth.com>
2019-07-07 00:46:17 -0400

    Initial Commit

---
+++ FCS Mock Season Creator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FCS Mock Season Creator
+//
+//  Created by Tyler Cheek on 7/7/19.
+//  Copyright © 2019 Tyler Cheek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FCS Mock Season CreatorTests/FCS_Mock_Season_CreatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  FCS_Mock_Season_CreatorTests.swift
+//  FCS Mock Season CreatorTests
+//
+//  Created by Tyler Cheek on 7/7/19.
+//  Copyright © 2019 Tyler Cheek. All rights reserved.
+//
+
+import XCTest
+@testable import FCS_Mock_Season_Creator
+
+class FCS_Mock_Season_CreatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
